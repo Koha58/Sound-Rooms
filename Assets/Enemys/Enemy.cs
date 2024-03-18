57b7a5c409f11bc274d35b1c84853144b7b74b07
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-03-18 20:39:32 +0900

    Enemyのキャラクターモデル,壁すり抜け落下防止

---
+++ Assets/Enemys/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // public GameObject characterPrefab; // 生成するキャラクターのプレハブ
+    float speed = 3f;
+    static public Vector3 targetPosition;
+
+    public Transform Player;//プレイヤーを参照
+    float Detection = 5f; //プレイヤーを検知する範囲
+    float ChaseSpeed = 1f;//追いかけるスピード
+
+
+
+    // [SerializeField] GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期位置をランダムに設定する
+        targetPosition = GetRandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float detectionPlayer = Vector3.Distance(transform.position, Player.position);//プレイヤーと敵の位置の計算
+
+        if (detectionPlayer <= Detection)//プレイヤーが検知範囲に入ったら
+        {
+            transform.LookAt(Player.transform); //プレイヤーの方向にむく
+            transform.position += transform.forward * ChaseSpeed;//プレイヤーの方向に向かう
+
+        }
+
+
+        // targetPositionに向かって移動する
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        // targetPositionに到着したら新しいランダムな位置を設定する
+        if (transform.position == targetPosition)
+        {
+            targetPosition = GetRandomPosition();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static Vector3 GetRandomPosition()
+    {
+        // ランダムなx, y, z座標を生成する
+        float randomX = Random.Range(-10f, 10f);
+        float randomY = 0f;// Random.Range(-10f, 10f);
+        float randomZ = Random.Range(-10f, 10f);
+
+        // 生成した座標を返す
+        return new Vector3(randomX, randomY, randomZ);
+    }
+}
