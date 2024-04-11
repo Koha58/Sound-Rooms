0c15611c0dfc4f26935fe37e0ffdd3e23a84a08a
Koharu <85koha90@gmail.com>
2024-04-11 14:55:45 +0900

    長押し中にメモリが減り続ける挙動を修正。 長押ししてメモリを一定数消費後、再度長押しするとメモリが異常に回復する挙動を修正中。 長押し中に広がる音波を作成。 プレイヤーの音波用のSphereの挙動がおかしかったので修正。

--- Assets/BoundScript.cs
+++ Assets/BoundScript.cs
@@ -6,10 +6,26 @@ public class BoundScript : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody Sphere; // ←バウンドさせたいオブジェクト
+    ButtonHoldDown BD;
+    GameObject hobj;
+    Rigidbody rb;
+    private float bounce = 4.0f;
 
-    private void OnCollisionEnter(Collision collision)
+    void Start()
     {
-        Sphere.AddForce(Vector3.up * 2f, ForceMode.Impulse);
+        rb = GetComponent<Rigidbody>();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        hobj = GameObject.Find("GaugeManager");
+        BD = hobj.GetComponent<ButtonHoldDown>(); //付いているスクリプトを取得
+        if (collision.gameObject.name == "Plane")
+        {
+            //if (BD.isOn == 1 && BD.MaxSound.activeSelf == false)
+           // {
+                rb.AddForce(transform.up.normalized * bounce, ForceMode.VelocityChange);
+          //  }
+        }
+    }
 }
