3922c37efa8099049d7a2c2fbf2ec5d54407ab3c
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-04-23 22:00:55 +0900

    Enemyが出す音をサンプルを入れました（まだEnemyから音を出す仕組みを作っていないので出せません）、Memoはスペースキーを押している間マイクが機能して音を出すスクリプトです（参考程度に良かったらどうぞ)(∩_∩)

---
+++ Assets/Memo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memo : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(null, true, 10, AudioSettings.outputSampleRate);
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Microphone.IsRecording(null))
+            {
+                Microphone.End(null);
+                audioSource.Stop();
+
+            }
+            else
+            {
+                audioSource.clip = Microphone.Start(null, true, 10, AudioSettings.outputSampleRate);
+                audioSource.loop = true;
+                audioSource.Play();
+
+            }
+        }
+    }
+}
