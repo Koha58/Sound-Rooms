2489f7ae008b3ac08a1e227718f43789b54be3be
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-05-19 14:51:57 +0900

    Enemysのスクリプトの最適化とEnemyの姿を現す時間をバラバラにする事に成功したがまだEnemyたちには入れていません

--- Assets/Enemys/Enemys.cs
+++ Assets/Enemys/Enemys.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Enemys : MonoBehaviour
@@ -8,7 +9,7 @@ public class Enemys : MonoBehaviour
     public Transform Player;//プレイヤーを参照
     Vector3 targetPosition;//Enemyの目的地
     float ChaseSpeed = 0.025f;//Playerを追いかけるスピード
-    private float Detection = 6f; //プレイヤーを検知する範囲
+    private float Detection = 5f; //プレイヤーを検知する範囲
     private float detectionPlayer;//プレイヤーと敵の位置の計算を格納する値
     private bool EnemyChaseOnOff = false;//Playerの追跡のONOFF 
 
@@ -22,29 +23,162 @@ public class Enemys : MonoBehaviour
 
     public Animator animator; //アニメーションの格納
 
-    //public GameObject eobj;
-   // public EnemySeens ES; // EnemySeenに付いているスクリプトを取得
+    float ONoff = 0;//(0が見えない；１が見える状態）
+    private float Seetime;  //経過時間
+    private float SoundTime;//経過時間
+    [SerializeField] public GameObject Sphere;
+    [SerializeField] public Transform _parentTransform;
 
+    float Enemystoptime = 0;
+    float Enemystoponoff;
+
+    float TargetTime;
+
+    [SerializeField]
+    private AudioClip SoundAttck;     //音を出すのオーディオクリップ
+    [SerializeField]
+    private  AudioClip footstepSound;     // 足音のオーディオクリップ
+    [SerializeField]
+    private  AudioSource audioSource;     // オーディオソース
+    [SerializeField]
+    private  float volume = 50f;          // 音量
+   
+    
+    public bool Soundonoff = true;
+
+    private Vector3 GetRandomPosition()
+    {
+        // ランダムなx, z座標を生成する
+        float randomX = Random.Range(-46f, 46f);
+        float randomY = 0f;// Random.Range(-10f, 10f);
+        float randomZ = Random.Range(-46f, 46f);
+
+        // 生成した座標を返す
+        return new Vector3(randomX, randomY, randomZ);
+    }
+
+    private void Increase()
+    {
+        if (isHiddens == false)
+        {
+            isHiddens = true;
+            GameObject go = Instantiate(ebiPrefab);//コピーを生成
+            //Debug.Log(go);
+            int px = Random.Range(0, 20);//0以上２０以下のランダムの値を生成
+            int pz = Random.Range(0, 20);//0以上２０以下のランダムの値を生成
+            go.transform.position = new Vector3(px, 0, pz);
+            Clone = true;
+        }
+
+        if (Clone == true)
+        {
+            Destroy(DestroyPrefab);
+            Clone = false;
+            enemyDeathcnt++;
+        }
+    }
+
+    private void Sound()
+    {
+        if (ONoff == 0)//EnemyChaseG1.detectionPlayerG1 <= EnemyChaseG1.Detection)
+        {
+            if (Soundonoff == true)
+            {
+                audioSource.clip = footstepSound;
+                audioSource.Play();
+            }
+        }
+        if (ONoff == 1)
+        {
+            if (Soundonoff == false)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+
+    private void AttackSiund()
+    {
+        if (ONoff == 1)//EnemyChaseG1.detectionPlayerG1 <= EnemyChaseG1.Detection)
+        {
+            if (Soundonoff == true)
+            {
+                audioSource.clip = SoundAttck;
+                audioSource.Play();
+            }
+        }
+        if (ONoff == 0)
+        {
+            if (Soundonoff == false)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        // 初期位置をランダムに設定する
-        targetPosition = GetRandomPosition();
+        targetPosition = GetRandomPosition();// 初期位置をランダムに設定する
         animator = GetComponent<Animator>();   //アニメーターコントローラーからアニメーションを取得する
 
+        //tagが"EnemyParts"である子オブジェクトのTransformのコレクションを取得
+        var childTransforms = _parentTransform.GetComponentsInChildren<Transform>().Where(t => t.CompareTag("EnemyParts"));
+
+        foreach (var item in childTransforms)
+        {
+            //タグが"EnemyParts"である子オブジェクトを見えなくする
+            item.gameObject.GetComponent<Renderer>().enabled = false;
+        }
     }
 
     // Update is called once per frame
     private void Update()
     {
+        Sound();
+
+        AttackSiund();
+
+        float randomTime = Random.Range(5f, 10f);
+
+        TargetTime = randomTime;
+
+        //tagが"EnemyParts"である子オブジェクトのTransformのコレクションを取得
+        var childTransforms = _parentTransform.GetComponentsInChildren<Transform>().Where(t => t.CompareTag("EnemyParts"));
+        if (ONoff == 0)//見えないとき
+        {
+            SoundTime += Time.deltaTime;
+            if (SoundTime >= TargetTime)
+            {
+                foreach (var item in childTransforms)
+                {
+                    //タグが"EnemyParts"である子オブジェクトを見えるようにする
+                    item.gameObject.GetComponent<Renderer>().enabled = true;
+                }
+                ONoff = 1;
+                SoundTime = 0.0f;
+                Sphere.SetActive(true);//音波非表示→表示
+            }
+        }
+        if (ONoff == 1)//見えているとき
+        {
+            Seetime += Time.deltaTime;
+            if (Seetime >= 10.0f)
+            {
+                foreach (var item in childTransforms)
+                {
+                    //タグが"EnemyParts"である子オブジェクトを見えなくする
+                    item.gameObject.GetComponent<Renderer>().enabled = false;
+                }
+                ONoff = 0;
+                Seetime = 0.0f;
+                Sphere.SetActive(false );//音波表示→非表示
+            }
+        }
+
         GameObject obj = GameObject.Find("Player"); //Playerオブジェクトを探す
         PlayerSeen PS = obj.GetComponent<PlayerSeen>(); //付いているスクリプトを取得
 
-        GameObject eobj = GameObject.FindWithTag("Enemy");
-        EnemySeens ES = eobj.GetComponent<EnemySeens>(); // EnemySeenに付いているスクリプトを取得
-
-       
         // 「歩く」のアニメーションを再生する
         animator.SetBool("EnemyWalk", true);
 
@@ -61,7 +195,7 @@ public class Enemys : MonoBehaviour
                 PS.onoff = 1;  //見えているから1
             }
 
-            if (PS.onoff == 1 && EnemyChaseOnOff == true && ES.ONoff == 1)
+            if (PS.onoff == 1 && EnemyChaseOnOff == true && ONoff == 1)
             {
                 transform.LookAt(Player.transform); //プレイヤーの方向にむく
                 transform.position += transform.forward * ChaseSpeed;//プレイヤーの方向に向かう
@@ -75,51 +209,35 @@ public class Enemys : MonoBehaviour
             transform.LookAt(targetPosition);
         }
 
+        // targetPositionに到着したら新しいランダムな位置を設定する
+        if (transform.position == targetPosition)
+        {
+            Enemystoponoff = 1;
+            if (Enemystoponoff == 1)
+            {
+                animator.SetBool("EnemyWalk", false);
+                Enemystoptime += Time.deltaTime;
+                if (Enemystoptime > 2.0f)
+                {
+                    targetPosition = GetRandomPosition();
+                    Enemystoponoff = 0;
+                }
+            }
+        }
+
+        Increase();
      
-        if (isHiddens == false)
-        {
-            isHiddens = true;
-            GameObject go = Instantiate(ebiPrefab);//コピーを生成
-            //Debug.Log(go);
-            int px = Random.Range(0, 20);//0以上２０以下のランダムの値を生成
-            int pz = Random.Range(0, 20);//0以上２０以下のランダムの値を生成
-            go.transform.position = new Vector3(px, 0, pz);
-            Clone = true;
-
-        }
-
-        if (Clone == true)
-        {
-            Destroy(DestroyPrefab);
-            Clone = false;
-            enemyDeathcnt++;
-        }
-
     }
-
-    private Vector3 GetRandomPosition()
-    {
-        // ランダムなx, z座標を生成する
-        float randomX = Random.Range(-46f, 46f);
-        float randomY = 0f;// Random.Range(-10f, 10f);
-        float randomZ = Random.Range(-46f, 46f);
-
-        // 生成した座標を返す
-        return new Vector3(randomX, randomY, randomZ);
-    }
+   
     private void OnTriggerEnter(Collider other)
-    {
-        GameObject eobj = GameObject.FindWithTag("Enemy");
-        EnemySeens ES = eobj.GetComponent<EnemySeens>(); // EnemySeenに付いているスクリプトを取得
-
+    { 
         if (other.gameObject.CompareTag("Player"))
         {
             detectionPlayer = Vector3.Distance(transform.position, Player.position);//プレイヤーと敵の位置の計算
 
-            if (detectionPlayer <= Detection && ES.ONoff == 1)//Enemyが可視化状態かつプレイヤーが検知範囲に入ったら
+            if (detectionPlayer <= Detection && ONoff == 1)//Enemyが可視化状態かつプレイヤーが検知範囲に入ったら
             {
                 EnemyChaseOnOff = true;
-
             }
         }
     }
