52056b3bb3cbe9daab6e12c8f660e5156ac7d7e1
STUDENT\2312004 <85koha90@gmail.com>
2024-05-24 16:44:55 +0900

    敵を倒した数に応じてバウンドの高さが変わるように修正。 いずれ消去する予定の機能のため、細かい円(バウンド)のずれがあり。

--- Assets/Pseen/BoundScript.cs
+++ Assets/Pseen/BoundScript.cs
@@ -9,17 +9,32 @@ public class BoundScript : MonoBehaviour
     ButtonHoldDown BD;
     GameObject hobj;
     Rigidbody rb;
-    private float bounce = 4.0f;
+    private float bounce = 1.5f;
+    private float Sbounce = 2.3f;
+    private float Mbounce = 2.8f;
+    private float Lbounce = 5.0f;
+    private int preBounce;//敵を倒した判定用
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        preBounce = 0;
     }
 
     void OnCollisionEnter(Collision collision)
     {
         hobj = GameObject.Find("GaugeManager");
         BD = hobj.GetComponent<ButtonHoldDown>(); //付いているスクリプトを取得
+
+        if(Enemyincrease.enemyDeathcnt > preBounce)
+        {
+            bounce += 0.5f;
+            Sbounce += 2.0f;
+            Mbounce += 2.0f;
+            Lbounce += 3.0f;
+            preBounce++;
+        }
+
         if (collision.gameObject.name == "Plane")
         {
             if (BD.boundHeight == 0)
@@ -28,15 +43,15 @@ public class BoundScript : MonoBehaviour
             }
             else if(BD.boundHeight == 1) 
             {
-                rb.AddForce(transform.up.normalized * bounce*1.3f, ForceMode.VelocityChange);
+                rb.AddForce(transform.up.normalized * Sbounce, ForceMode.VelocityChange);
             }
             else if (BD.boundHeight == 2)
             {
-                rb.AddForce(transform.up.normalized * bounce * 1.5f, ForceMode.VelocityChange);
+                rb.AddForce(transform.up.normalized * Mbounce, ForceMode.VelocityChange);
             }
             else if (BD.boundHeight == 3)
             {
-                rb.AddForce(transform.up.normalized * bounce * 2.5f, ForceMode.VelocityChange);
+                rb.AddForce(transform.up.normalized * Lbounce, ForceMode.VelocityChange);
             }
 
         }
