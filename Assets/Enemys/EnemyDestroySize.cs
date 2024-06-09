a29c76140163d19ae340bc5526394aaddc890214
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-06-09 22:13:52 +0900

    Enemyのリングとアニメーションの調整、椅子を入れました。

--- Assets/Enemys/EnemyDestroySize.cs
+++ Assets/Enemys/EnemyDestroySize.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class EnemyDestroySize : MonoBehaviour
 {
+    float i;
     // Start is called before the first frame update
     void Start()
     {
-       
+        StartCoroutine("ScaleUp");
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-        for (float i = 1; i < 5; i += 0.1f)
+
+        GameObject eobj = GameObject.FindWithTag("Enemy");
+        EnemyFailurework EF = eobj.GetComponent<EnemyFailurework>(); //Enemyに付いているスクリプトを取得
+        if(EF.ONoff == 1)
+        {
+           // StartCoroutine("ScaleUp");
+        }
+
+    }
+
+    IEnumerator ScaleUp()
+    {
+        for (i = 1; i < 3; i += 0.1f)
         {
             this.transform.localScale = new Vector3(i, i, i);
+            yield return new WaitForSeconds(0.1f);
         }
-        
     }
 }
