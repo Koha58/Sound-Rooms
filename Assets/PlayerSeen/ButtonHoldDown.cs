5fc9259296671e7c29fcfa49353c01732fc995d8
Koharu <85koha90@gmail.com>
2024-06-17 23:00:48 +0900

    使わないオブジェクト、スクリプトを削除。 声の大きさに合わせて、SeenAreaや円の大きさが変わるようにした。

--- Assets/PlayerSeen/ButtonHoldDown.cs
+++ Assets/PlayerSeen/ButtonHoldDown.cs
@@ -6,137 +6,63 @@ using UnityEngine.UI;
 //マウス長押し挙動(範囲指定)
 public class ButtonHoldDown : MonoBehaviour
 {
-    public GameObject[] GaugeArray = new GameObject[13];//右上のゲージのメモリを格納している配列
-    private int Gauge = 13;
-    //長押しと判定するフレーム数を管理
-    private const int holdCheck = 60;
-    //キーを押しているフレーム数を記録
-    private float holdTime = 0;
-    private float recoveryTime = 0;//回復までの時間
     public GameObject MaxSound;//音の広がりの最大値
-    public GameObject[] SoundArray = new GameObject[3];//音の広がりの円を格納している配列
-    private int SoundSize = 3;
-    PlayerSeen PS;
-    GameObject bobj;
-    private int count;
-    public int isOn =0;
-    public int boundHeight =0;
-    private float originSizemX = 5.5f;
-    private float originSizemZ = 5.5f;
-    private float originSizeX = 3.5f;
-    private float originSizeZ = 3.5f;
-    private float originSize2X = 4.5f;
-    private float originSize2Z = 4.5f;
-    private float originSize3X = 5.5f;
-    private float originSize3Z = 5.5f;
+    public GameObject Sound; //音の広がりの円
+
+    //音の広がりの最大値のサイズ
+    private float originSizemX = 10.3f;
+    private float originSizemZ = 10.3f;
+    //音の広がりのサイズ
+    private float originSizeX = 2.3f;
+    private float originSizeZ = 2.3f;
+
+    LevelMeter levelMeter;
+
+    // 変更前の値
+    private int preHeight;
+    //敵を倒すごとに増加する変数
+    private float plusSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        GaugeArray[Gauge-1].GetComponent<Image>().enabled = true;
-        SoundArray[SoundSize-1].SetActive(false);
-        SoundArray[SoundSize-2].SetActive(false);
-        SoundArray[SoundSize-3].SetActive(false);
+        Sound.SetActive(false);
         MaxSound.SetActive(false);
+
+        preHeight = 0;
+        plusSize = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bobj = GameObject.Find("Player");
-        PS = bobj.GetComponent<PlayerSeen>(); //付いているスクリプトを取得
-        count += 1;
-        if (PS.onoff == 0)
+        GameObject soundobj = GameObject.Find("SoundVolume");
+        levelMeter = soundobj.GetComponent<LevelMeter>(); //付いているスクリプトを取得
+
+        if (levelMeter.nowdB > 0.0f)
         {
-            if (Input.GetMouseButtonDown(0) && Gauge > 0 && holdCheck > holdTime)//短押し
+            if (preHeight != Enemyincrease.enemyDeathcnt)
             {
-                holdTime = 0;
-                GaugeArray[Gauge - 1].GetComponent<Image>().enabled = false;
-                Gauge--;
-                boundHeight = 0;
+                plusSize += 1.0f;
+
+                preHeight++;
             }
 
-            if (Input.GetMouseButton(0) && Gauge > 0)//leftキーを押している間の時間計測
-            {
-                holdTime += Time.deltaTime;
-            }
+            originSizeX = (levelMeter.nowdB * 20) + plusSize;
+            originSizeZ = (levelMeter.nowdB * 20) + plusSize;
 
-            if (Input.GetMouseButton(0) && Gauge > 0 && holdTime > 1)//leftキーを押している間
-            {
-                holdTime = 0;
-                GaugeArray[Gauge - 1].GetComponent<Image>().enabled = false;
-                Gauge--;
+            Sound.transform.localScale = new Vector3(originSizeX, 1, originSizeZ);
 
-                if (Enemyincrease.DeathRange == 0)
-                {
-                    MaxSound.transform.localScale = new Vector3(originSizemX, 1.0f, originSizemZ);
-                    MaxSound.SetActive(true);
-                    SoundArray[SoundSize - 1].transform.localScale = new Vector3(originSizeX, 1.0f, originSizeZ);
-                    SoundArray[SoundSize - 2].transform.localScale = new Vector3(originSize2X, 1.0f, originSize2Z);
-                    SoundArray[SoundSize - 3].transform.localScale = new Vector3(originSize3X, 1.0f, originSize3Z);
-                }
-                else
-                {
-                    MaxSound.transform.localScale = new Vector3(originSizemX + (3 * Enemyincrease.DeathRange), 1.0f, originSizemZ + (3 * Enemyincrease.DeathRange));
-                    MaxSound.SetActive(true);
-                    SoundArray[SoundSize - 1].transform.localScale = new Vector3(originSizeX + Enemyincrease.DeathRange, 1.0f, originSizeZ + Enemyincrease.DeathRange);
-                    SoundArray[SoundSize - 2].transform.localScale = new Vector3(originSize2X + (2 * Enemyincrease.DeathRange), 1.0f, originSize2Z + (2 * Enemyincrease.DeathRange));
-                    SoundArray[SoundSize - 3].transform.localScale = new Vector3(originSize3X + (3 * Enemyincrease.DeathRange), 1.0f, originSize3Z + (3 * Enemyincrease.DeathRange));
-                }
+            originSizemX = originSizemX + plusSize;
+            originSizemZ = originSizemZ + plusSize;
 
-                if (count % 1 == 0)
-                {
-                    if (isOn == 0)
-                    {
-                        if (boundHeight == 3)
-                        {
-                            GaugeArray[Gauge].GetComponent<Image>().enabled = true;
-                            GaugeArray[Gauge + 1].GetComponent<Image>().enabled = true;
-                            GaugeArray[Gauge + 2].GetComponent<Image>().enabled = true;
-                            Gauge += 3;
-                        }
-
-                        SoundArray[SoundSize - 1].SetActive(true);
-                        SoundArray[SoundSize - 2].SetActive(false);
-                        SoundArray[SoundSize - 3].SetActive(false);
-                        isOn = 1;
-                        boundHeight = 1;
-                    }
-                    else if (isOn == 1)
-                    {
-                        SoundArray[SoundSize - 1].SetActive(false);
-                        SoundArray[SoundSize - 2].SetActive(true);
-                        SoundArray[SoundSize - 3].SetActive(false);
-                        isOn = 2;
-                        boundHeight = 2;
-                    }
-                    else if (isOn == 2)
-                    {
-                        SoundArray[SoundSize - 1].SetActive(false);
-                        SoundArray[SoundSize - 2].SetActive(false);
-                        SoundArray[SoundSize - 3].SetActive(true);
-                        isOn = 0;
-                        boundHeight = 3;
-                    }
-                }
-            }
+            Sound.SetActive(true);
+            MaxSound.SetActive(true);
         }
-        if (Input.GetMouseButtonUp(0))
+        else
         {
+            Sound.SetActive(false);
             MaxSound.SetActive(false);
-            SoundArray[SoundSize - 1].SetActive(false);
-            SoundArray[SoundSize - 2].SetActive(false);
-            SoundArray[SoundSize - 3].SetActive(false);
-            isOn = 0;
-        }
-
-        recoveryTime += Time.deltaTime;
-        
-        if(Gauge < 13 && recoveryTime > 60)//60秒ごとに「１」回復
-        {
-            recoveryTime = 0;
-            GaugeArray[Gauge].GetComponent<Image>().enabled = true;
-            Gauge++;
         }
     }
 }
--- Assets/PlayerSeen/SeenRange.cs
+++ Assets/PlayerSeen/SeenRange.cs
@@ -5,60 +5,41 @@ using UnityEngine;
 //SeenAreaのサイズ変更
 public class SeenRange : MonoBehaviour
 {
-    private float originSizeX = 2.3f;
+    private float originSizeX;
     private float originSizeY = 2.0f;
     private float originSizeZ = 2.0f;
-    private float originSizeSX = 3.3f;
-    private float originSizeSY = 2.0f;
-    private float originSizeSZ = 2.0f;
-    private float originSizeMX = 4.3f;
-    private float originSizeMY = 2.0f;
-    private float originSizeMZ = 2.0f;
-    private float originSizeLX = 5.3f;
-    private float originSizeLY = 2.0f;
-    private float originSizeLZ = 2.0f;
+
     // 変更前の値
     private int preHeight;
-    ButtonHoldDown BD;
-   
+    //敵を倒すごとに増加する変数
+    private float plusSize;
+
+    LevelMeter levelMeter;
+
 
     // Start is called before the first frame update
     void Start()
     {
         preHeight = 0;
+        plusSize = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject hobj = GameObject.Find("GaugeManager");
-        BD = hobj.GetComponent<ButtonHoldDown>(); //付いているスクリプトを取得
+        GameObject soundobj = GameObject.Find("SoundVolume");
+        levelMeter = soundobj.GetComponent<LevelMeter>(); //付いているスクリプトを取得
 
         if (preHeight != Enemyincrease.enemyDeathcnt)
         {
-            originSizeX += 1.0f;
-            originSizeSX += 1.0f;
-            originSizeMX += 2.0f;
-            originSizeLX += 3.0f;
+            plusSize += 1.0f;
 
             preHeight++;
         }
 
-        if(BD.boundHeight == 0)
-        {
-            transform.localScale = new Vector3(originSizeX, originSizeY, originSizeZ);
-        }
-        else if (BD.boundHeight == 1)
-        {
-            transform.localScale = new Vector3(originSizeSX, originSizeSY, originSizeSZ);
-        }
-        else if (BD.boundHeight == 2)
-        {
-            transform.localScale = new Vector3(originSizeMX, originSizeMY, originSizeMZ);
-        }
-        else
-        {
-            transform.localScale = new Vector3(originSizeLX, originSizeLY, originSizeLZ);
-        }
+        originSizeX = (levelMeter.nowdB * 20) + plusSize;
+
+        transform.localScale = new Vector3(originSizeX, originSizeY, originSizeZ);
+
     }
 }
