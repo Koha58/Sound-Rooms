5a596fc5eb1de89d5269ab8350daa84d1d1dfcbd
Koharu <85koha90@gmail.com>
2024-07-07 02:16:36 +0900

    Attentionをいつでも見れるよう、UIに追加。

--- Assets/ButtonScript/GameSceneButton.cs
+++ Assets/ButtonScript/GameSceneButton.cs
@@ -9,6 +9,13 @@ public class GameSceneButton : MonoBehaviour
     private GameObject OperationExplanation;
     private GameObject closeButton;
 
+    private GameObject AttentionUI;
+    private GameObject AttentionTextUI;
+    private GameObject AttentionButton;
+
+    public bool needAttention = false;
+    private float AttentionCount = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +24,28 @@ public class GameSceneButton : MonoBehaviour
 
         closeButton = GameObject.Find("closeButton");
         closeButton.GetComponent<Image>().enabled = false;
+
+        AttentionUI = GameObject.Find("AttentionUI");
+        AttentionUI.GetComponent<Image>().enabled = false;
+
+        AttentionTextUI = GameObject.Find("AttentionTextUI");
+        AttentionTextUI.GetComponent<Image>().enabled = false;
+
+        AttentionButton = GameObject.Find("AttentionButton");
+        AttentionButton.GetComponent<Image>().enabled = false;
+
+        needAttention = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AttentionCount += Time.deltaTime;
+        if (AttentionCount > 30)
+        {
+            needAttention = true;
+        }
+
         if (Input.GetKeyDown("joystick button 7"))
         {
             OperationExplanation.GetComponent<Image>().enabled = true;
@@ -33,6 +57,13 @@ public class GameSceneButton : MonoBehaviour
             closeButton.GetComponent<Image>().enabled = false;
             OperationExplanation.GetComponent<Image>().enabled = false;
         }
+
+        if (needAttention)
+        {
+            AttentionTextUI.GetComponent<Image>().enabled = true;
+            AttentionButton.GetComponent<Image>().enabled = true;
+        }
+
     }
 
     public void SettingButton()
@@ -45,5 +76,13 @@ public class GameSceneButton : MonoBehaviour
     {
         closeButton.GetComponent<Image>().enabled = false;
         OperationExplanation.GetComponent<Image>().enabled = false;
+        AttentionUI.GetComponent<Image>().enabled = false;
+        needAttention = false;
+    }
+
+    public void AttentionUIButton()
+    {
+        AttentionUI.GetComponent<Image>().enabled = true;
+        closeButton.GetComponent<Image>().enabled = true;
     }
 }
