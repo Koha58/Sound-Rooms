e69c5e0d43ddf242da5e4495667eea12562d0a0e
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-07-07 14:45:55 +0900

    音を入れた時の挙動の確認、（音割れ酷い、直す）

--- Assets/Enemys/EnemySound1.cs
+++ Assets/Enemys/EnemySound1.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class EnemySound1 : MonoBehaviour
 {
-    public AudioClip footstepSound;     // 足音のオーディオクリップ
+    public AudioClip Sound;     // 足音のオーディオクリップ
+    public AudioClip Sound1;     // 足音のオーディオクリップ
     public AudioSource audioSource;     // オーディオソース
     public float volume = 1f;          // 音量
 
     private void Start()
     {
-        audioSource.clip = footstepSound;
+
     }
 
     private void Update()
     {
         GameObject eobj = GameObject.FindWithTag("Enemy");
         EnemyController EC = eobj.GetComponent<EnemyController>();
+        if (EC.ONoff == 0)
+        {
+            audioSource.clip = Sound;
+            audioSource.Play();
+        }
         if (EC.ONoff == 1)
         {
-            audioSource.clip = footstepSound;
+            audioSource.clip = Sound1;
+            audioSource.Play();
         }
     }
 
