d70db629a04efa2ca488ae46167551862d7adadc
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-07-09 21:30:43 +0900

    Enemy増やす途中

--- Assets/Enemys/Enemy3/Enemy3Sound1.cs
+++ Assets/Enemys/Enemy3/Enemy3Sound1.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Enemy3Sound1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // public AudioClip Sound;     // 足音のオーディオクリップ
+    public AudioClip Sound1;     // 足音のオーディオクリップ
+    public AudioSource audioSource;     // オーディオソース
+    public float volume = 40f;          // 音量
+
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        audioSource.clip = Sound1;
+        GameObject eobj3 = GameObject.FindWithTag("Enemy3");
+        EnemyController3 EC3 = eobj3.GetComponent<EnemyController3>();
+        if (EC3.ONoff == 0)
+        {
+            audioSource.mute = false;
+            audioSource.loop = true;
+            audioSource.volume = volume;
+        }
+
+        if (EC3.ONoff == 1)
+        {
+            audioSource.mute = true;
+        }
     }
+
 }
