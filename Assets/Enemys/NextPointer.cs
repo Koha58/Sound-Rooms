2dcaf851687438c52ff2f0f7c5ee18428841b0d7
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-07-12 09:14:55 +0900

    追加

--- Assets/Enemys/NextPointer.cs
+++ Assets/Enemys/NextPointer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
 
 public class NextPointer : MonoBehaviour
 {
