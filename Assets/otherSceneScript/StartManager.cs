ed13e9ba5864d69ae4c65e2bc41d0ef128b5ef58
Koharu <85koha90@gmail.com>
2024-08-07 22:12:17 +0900

    StartSceneのUI変更。 カーソルが合っているときに明るく表示されるようにした。
    コントローラー操作対応よろしくお願いします('◇')ゞ

--- Assets/otherSceneScript/StartManager.cs
+++ Assets/otherSceneScript/StartManager.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartManager : MonoBehaviour
 {
+    Image StartButtonImage;
+    Image TutorialButtonImage;
+
+    public GameObject StartButton;
+    public GameObject TutorialButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartButtonImage = StartButton.GetComponent<Image>();
+        TutorialButtonImage = TutorialButton.GetComponent<Image>();
+
+        StartButtonImage.color = new Color32(255, 255, 255, 45);
+        TutorialButtonImage.color = new Color32(255, 255, 255, 45);
+
     }
 
     // Update is called once per frame
@@ -23,7 +35,26 @@ public class StartManager : MonoBehaviour
 
     public void OnStart()
     {
-        //StartSound.PlayOneShot(StartSound.clip);
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void EnterStartButton()
+    {
+        StartButtonImage.color = new Color32(255, 255, 255, 255);
+    }
+
+    public void ExitStartButton()
+    {
+        StartButtonImage.color = new Color32(255, 255, 255, 45);
+    }
+
+    public void EnterTutorialButton()
+    {
+        TutorialButtonImage.color = new Color32(255, 255, 255, 255);
+    }
+
+    public void ExitTutorialButton()
+    {
+        TutorialButtonImage.color = new Color32(255, 255, 255, 45);
     }
 }
