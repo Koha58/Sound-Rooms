69016ed7be1775fefd6d5204b52d566e494fc897
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-08-20 16:33:15 +0900

    Enemyのエラーを出さないようにしました 復活方法を少し変更、それぞれのリスポーン地点から復活する方式にしました。

--- Assets/Enemys/Prototype/Enemy/EnemyIncrease.cs
+++ Assets/Enemys/Prototype/Enemy/EnemyIncrease.cs
@@ -12,6 +12,24 @@ public class EnemyIncrease : MonoBehaviour
     static public int enemyDeathcnt = 0;  //Enemyが死んだ数
     public static float DeathRange = 0f;//Enemyが死ぬと広がる範囲
 
+
+    public GameObject enemyPrefab; // 復活させる敵のプレハブ
+    public Transform respawnPoint; // 復活させる位置
+    public float respawnDistance = 20f; // 復活までの距離
+
+    private GameObject enemyInstance; // 生成された敵のインスタンス
+
+    // 敵が倒された時に呼ばれるメソッド
+    public void EnemyDied()
+    {
+        // 一定距離以内で敵を復活させる
+        if (Vector3.Distance(respawnPoint.position, transform.position) <= respawnDistance)
+        {
+            // 復活位置に敵を生成する
+            enemyInstance = Instantiate(enemyPrefab, respawnPoint.position, respawnPoint.rotation);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +39,22 @@ public class EnemyIncrease : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+
         if (isHidden == false)
         {
             isHidden = true;
+            EnemyDied();
+            /*
             GameObject go1 = Instantiate(ebiPrefab1);//コピーを生成
             GameObject go2 = Instantiate(ebiPrefab2);//コピーを生成
                                                      //Debug.Log(go);
-            float px1 = Random.Range(-10f, 10f); ;//0以上２０以下のランダムの値を生成
-            float pz1 = Random.Range(-10f, 10f); ;//0以上２０以下のランダムの値を生成
-            float px2 = Random.Range(-10f, 10f); ;//0以上２０以下のランダムの値を生成
-            float pz2 = Random.Range(-10f, 10f); ;//0以上２０以下のランダムの値を生成
+            float px1 = Random.Range(-15f, 15f); ;//0以上２０以下のランダムの値を生成
+            float pz1 = Random.Range(-15f, 15f); ;//0以上２０以下のランダムの値を生成
+            float px2 = Random.Range(-15f, 15f); ;//0以上２０以下のランダムの値を生成
+            float pz2 = Random.Range(-15f, 15f); ;//0以上２０以下のランダムの値を生成
             go1.transform.position = new Vector3(px1, 0, pz1);
             go2.transform.position = new Vector3(px2, 0, pz2);
+            */
             Clone = true;
         }
 
@@ -43,6 +65,6 @@ public class EnemyIncrease : MonoBehaviour
             enemyDeathcnt++;
             DeathRange += 1.0f;
         }
-
+        
     }
 }
