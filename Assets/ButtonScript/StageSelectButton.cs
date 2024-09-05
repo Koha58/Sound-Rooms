1b8225bfadc999192235c694aa26a58867e38641
STUDENT\2312004 <85koha90@gmail.com>
2024-09-05 14:51:20 +0900

    StageSelectSceneのPC挙動完成。

--- Assets/ButtonScript/StageSelectButton.cs
+++ Assets/ButtonScript/StageSelectButton.cs
@@ -1,100 +1,263 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StageSelectButton : MonoBehaviour
 {
-    Image Stage0ButtonImage;
-    Image Stage1ButtonImage;
+    public GameObject[] StageButtons;
 
-    public GameObject Stage0Button;
-    public GameObject Stage1Button;
+    public GameObject RightButton;
+    public GameObject LeftButton;
 
     public GameObject Cursor;
     public GameObject Cursor1;
 
+    public GameObject[] StageVideos;
+    public GameObject[] StageTitles;
+
     bool UPDOWN;
+
+    int stage;
 
     // Start is called before the first frame update
     void Start()
     {
-        Stage0ButtonImage = Stage0Button.GetComponent<Image>();
-        Stage1ButtonImage = Stage1Button.GetComponent<Image>();
+        stage = 0;
+        StageButtons[stage].GetComponent<Image>().enabled = true;
 
-        Stage0ButtonImage.color = new Color32(255, 255, 255, 45);
-        Stage1ButtonImage.color = new Color32(255, 255, 255, 45);
+        for (int i = 0; i < StageButtons.Length; i++)
+        {
+            StageButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 45);
+        }
 
-        Cursor.SetActive(false);
-        Cursor1.SetActive(false);
+        StageButtons[stage].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+        for (int i = 0; i < StageVideos.Length; i++)
+        {
+            StageVideos[i].GetComponent<RawImage>().enabled = false;
+        }
+
+        StageVideos[stage].GetComponent<RawImage>().enabled = true;
+
+        for (int i = 0; i < StageTitles.Length; i++)
+        {
+            StageTitles[i].GetComponent<Image>().enabled = false;
+        }
+
+        StageTitles[stage].GetComponent<Image>().enabled = true;
+
+        Cursor.GetComponent<Image>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Transform cursorTransform = Cursor.transform;
+        //if (Input.GetAxisRaw("Vertical") < 0)
+        //{
+        //   if(stage == 0)
+        //    {
 
-        if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            Stage0ButtonImage.color = new Color32(255, 255, 255, 255);
-            Stage1ButtonImage.color = new Color32(255, 255, 255, 45);
-            Cursor.SetActive(true);
-            Cursor1.SetActive(false);
-            UPDOWN = true;
-        }
+        //    }
+        //    Stage0ButtonImage.color = new Color32(255, 255, 255, 255);
+        //    Stage1ButtonImage.color = new Color32(255, 255, 255, 45);
+        //    Cursor.SetActive(true);
+        //    Cursor1.SetActive(false);
+        //    UPDOWN = true;
+        //}
 
-        if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            Stage0ButtonImage.color = new Color32(255, 255, 255, 45);
-            Stage1ButtonImage.color = new Color32(255, 255, 255, 255);
-            Cursor1.SetActive(true);
-            Cursor.SetActive(false);
-            UPDOWN = false;
-        }
-        if (UPDOWN == true)
-        {
-            if (Input.GetKeyDown("joystick button 0"))
-            {
-                SceneManager.LoadScene("TutorialScene");
-            }
-        }
-        else
-        {
-            if (Input.GetKeyDown("joystick button 0"))
-            {
-                SceneManager.LoadScene("GameScene");
-            }
-        }
+        //if (Input.GetAxisRaw("Vertical") > 0)
+        //{
+        //    Stage0ButtonImage.color = new Color32(255, 255, 255, 45);
+        //    Stage1ButtonImage.color = new Color32(255, 255, 255, 255);
+        //    UPDOWN = false;
+        //}
+        //if (UPDOWN == true)
+        //{
+        //    if (Input.GetKeyDown("joystick button 0"))
+        //    {
+        //        SceneManager.LoadScene("TutorialScene");
+        //    }
+        //}
+        //else
+        //{
+        //    if (Input.GetKeyDown("joystick button 0"))
+        //    {
+        //        SceneManager.LoadScene("GameScene");
+        //    }
+        //}
+
+        StageSelect();
 
     }
 
     public void OnStage0Select()
     {
-        SceneManager.LoadScene("TutorialScene");
+        stage = 0;
+        for (int i = 0; i < StageButtons.Length; i++)
+        {
+            StageButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 45);
+        }
+
+        StageButtons[stage].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
     }
 
     public void OnStage1Select()
     {
-        SceneManager.LoadScene("GameScene");
+        stage = 1;
+        for (int i = 0; i < StageButtons.Length; i++)
+        {
+            StageButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 45);
+        }
+
+        StageButtons[stage].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
     }
 
     public void EnterStage0SelectButton()
     {
-        Stage0ButtonImage.color = new Color32(255, 255, 255, 255);
+        if(stage != 0)
+        {
+            StageButtons[0].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        }
     }
 
     public void ExitStage0SelectButton()
     {
-        Stage0ButtonImage.color = new Color32(255, 255, 255, 45);
+        if (stage != 0)
+        {
+            StageButtons[0].GetComponent<Image>().color = new Color32(255, 255, 255, 45);
+        }
     }
 
     public void EnterStage1SelectButton()
     {
-        Stage1ButtonImage.color = new Color32(255, 255, 255, 255);
+        if (stage != 1)
+        {
+            StageButtons[1].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        }
     }
 
     public void ExitStage1SelectButton()
     {
-        Stage1ButtonImage.color = new Color32(255, 255, 255, 45);
+        if (stage != 1)
+        {
+            StageButtons[1].GetComponent<Image>().color = new Color32(255, 255, 255, 45);
+        }
+    }
+
+    public void OnRightButton()
+    {
+        if(stage != 1)
+        {
+            stage++;
+        }
+        else
+        {
+            stage = 0;
+        }
+        for (int i = 0; i < StageButtons.Length; i++)
+        {
+            StageButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 45);
+        }
+
+        StageButtons[stage].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+        for (int i = 0; i < StageVideos.Length; i++)
+        {
+            StageVideos[i].GetComponent<RawImage>().enabled = false;
+        }
+
+        StageVideos[stage].GetComponent<RawImage>().enabled = true;
+
+        for (int i = 0; i < StageTitles.Length; i++)
+        {
+            StageTitles[i].GetComponent<Image>().enabled = false;
+        }
+
+        StageTitles[stage].GetComponent<Image>().enabled = true;
+    }
+
+    public void OnLeftButton()
+    {
+        if(stage != 0)
+        {
+            stage--;
+        }
+        else
+        {
+            stage = 1;
+        }
+        for (int i = 0; i < StageButtons.Length; i++)
+        {
+            StageButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 45);
+        }
+
+        StageButtons[stage].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+        for (int i = 0; i < StageVideos.Length; i++)
+        {
+            StageVideos[i].GetComponent<RawImage>().enabled = false;
+        }
+
+        StageVideos[stage].GetComponent<RawImage>().enabled = true;
+
+        for (int i = 0; i < StageTitles.Length; i++)
+        {
+            StageTitles[i].GetComponent<Image>().enabled = false;
+        }
+
+        StageTitles[stage].GetComponent<Image>().enabled = true;
+    }
+
+    void StageSelect()
+    {
+        if (stage == 0)
+        {
+            for (int i = 0; i < StageVideos.Length; i++)
+            {
+                StageVideos[i].GetComponent<RawImage>().enabled = false;
+            }
+
+            StageVideos[stage].GetComponent<RawImage>().enabled = true;
+
+            for (int i = 0; i < StageTitles.Length; i++)
+            {
+                StageTitles[i].GetComponent<Image>().enabled = false;
+            }
+
+            StageTitles[stage].GetComponent<Image>().enabled = true;
+        }
+        else if (stage == 1)
+        {
+            for (int i = 0; i < StageVideos.Length; i++)
+            {
+                StageVideos[i].GetComponent<RawImage>().enabled = false;
+            }
+
+            StageVideos[stage].GetComponent<RawImage>().enabled = true;
+
+            for (int i = 0; i < StageTitles.Length; i++)
+            {
+                StageTitles[i].GetComponent<Image>().enabled = false;
+            }
+
+            StageTitles[stage].GetComponent<Image>().enabled = true;
+        }
+    }
+
+    public void OnStart()
+    {
+        if(stage == 0)
+        {
+            SceneManager.LoadScene("TutorialScene");
+        }
+        else if(stage == 1)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 }
