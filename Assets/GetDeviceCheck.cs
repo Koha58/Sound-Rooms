508cbebb304ce8224242341d52d8f3aab0ac4801
STUDENT\2312004 <85koha90@gmail.com>
2024-10-08 14:58:07 +0900

    スピーカーの接続有無の確認途中。 スピーカーを接続できているときは確認できるようにした。

--- Assets/GetDeviceCheck.cs
+++ Assets/GetDeviceCheck.cs
@@ -2,29 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GetDeviceCheck : MonoBehaviour
 {
 
     bool micCheck = false;
 
-    bool speakerCheck = false;
-
-    public AudioSource audioSource; // InspectorでAudioSourceを指定
-
     // Start is called before the first frame update
     void Start()
     {
         micCheck = false;
-        speakerCheck = false;
         foreach (string device in Microphone.devices)
         {
             Debug.Log("Name: " + device);
             micCheck = true;
         }
-
-        //CheckAudioDevices();
     }
 
     // Update is called once per frame
@@ -39,8 +31,6 @@ public class GetDeviceCheck : MonoBehaviour
         {
             Debug.Log("マイクが接続されていません");
         }
-
-      
     }
 
 }
