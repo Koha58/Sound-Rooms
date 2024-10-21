5b0635e746cce48aae578fc2572390d5d415cedb
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-10-21 20:11:36 +0900

    Unityのシーン切り替えを便利にするエディターを追加、ゲーム、チュートリアル、スタート３つのシーンを楽に選べるようにしました。 Launcherの所でショートカットキー見れるから、あとは追加したり、するのも自由！ あとは好きにいじれ

---
+++ Assets/Scripts/Editor/EditorSceneLauncher.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+public static class EditorSceneLauncher
+{
+    [MenuItem("Launcher/GameScene", priority = 0)]
+    public static void OpenGameScene()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/GameScene.unity", OpenSceneMode.Single);
+    }
+
+    [MenuItem("Launcher/TutorialScene", priority = 0)]
+    public static void OpenSampleScene()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/TutorialScene.unity", OpenSceneMode.Single);
+    }
+
+    [MenuItem("Launcher/StartScene", priority = 0)]
+    public static void OpenTitleScene()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/StartScene.unity", OpenSceneMode.Single);
+    }
+}
