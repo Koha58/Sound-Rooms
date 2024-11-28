b7468d422a39a4b33198d4a876cdb401eacd01ba
四季社 <150815917+SikisyaTakuto@users.noreply.github.com>
2024-11-28 09:39:59 +0900

    Enemyの状態をそれぞれSwich文で変化出来るようにしました。まだ切り替えシステムを作っていないので、形としてあるだけです

--- Assets/Enemys/Enemy/EnemyController1.cs
+++ Assets/Enemys/Enemy/EnemyController1.cs
@@ -5,42 +5,7 @@ using UnityEngine;
 
 public class EnemyController1 : MonoBehaviour
 {
-    enum enemyState
-    { 
-        walk,
-        chase,
-        search,
-        doNothing
-    }
-
-    enemyState curretState = enemyState.doNothing;//現在のステートは何もしていない
-    bool stateEnter = true;                    　 //ステートの変化時に一回だけ特殊な処理をさせたいときに使用
-
-    void ChangeState(enemyState newEnemyState)
-    {
-        curretState = newEnemyState;
-        stateEnter = true;
-    }
-
-    private void Update()
-    {
-        //if(curretState != enemyState.walk) 
-        //{ 
-
-        //}
-
-        switch(curretState)
-        {
-            case enemyState.doNothing:
-                if(stateEnter)
-                {
-                    stateEnter = false;
-                    Debug.Log("何もしない");
-                }
-                break; 
-
-        }
-    }
+    private Transform player;
 
     //アニメーション
     [SerializeField] Animator animator;　//アニメーター取得
@@ -51,4 +16,97 @@ public class EnemyController1 : MonoBehaviour
     [SerializeField] AudioClip runClip;       //走る音
     [SerializeField] AudioClip walkClip;      //歩く音
 
+    //ステートベースAI
+    #region
+    enum enemyState
+    {
+        walk,
+        chase,
+        search,
+        doNothing
+    }
+
+    float walking = 0;//歩いている　0～１;
+
+    float chasing = 0;//追いかけている　0～１;
+
+    float searching = 0;//探している　0～１;
+
+    enemyState curretState = enemyState.doNothing;//現在のステートは何もしていない
+    bool stateEnter = true;                    　 //ステートの変化時に一回だけ特殊な処理をさせたいときに使用
+
+    void ChangeState(enemyState newEnemyState)
+    {
+        curretState = newEnemyState;
+        stateEnter = true;
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;　//プレイヤーの位置を取得
+    }
+
+    private void Update()
+    {
+        //if(curretState != enemyState.walk) 
+        //{ 
+
+        //}
+
+        switch (curretState)
+        {
+            case enemyState.doNothing:
+                #region
+                if (stateEnter)
+                {
+                    stateEnter = false;
+                    Debug.Log("何もしない");
+                }
+
+
+
+                #endregion
+                break;
+            case enemyState.search:
+                #region
+                if (stateEnter)
+                {
+                    stateEnter = false;
+                    Debug.Log("どこにいるかな？");
+                }
+
+
+
+                #endregion
+                break;
+            case enemyState.walk:
+                #region
+                if (stateEnter)
+                {
+                    stateEnter = false;
+                    Debug.Log("歩いている");
+                }
+
+
+
+                #endregion
+                break;
+            case enemyState.chase:
+                #region
+                if (stateEnter)
+                {
+                    stateEnter = false;
+                    Debug.Log("追いかけいるよ");
+                }
+
+
+
+                #endregion
+                break;
+
+
+        }
+    }
 }
