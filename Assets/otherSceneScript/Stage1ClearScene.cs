756e1eed17dec1bee2aea573b4dfec2771543ef2
STUDENT\2312004 <85koha90@gmail.com>
2025-01-07 16:40:51 +0900

    ClearrSceneに遷移後に青い画面がちらつく状況を修正途中。

--- Assets/otherSceneScript/Stage1ClearScene.cs
+++ Assets/otherSceneScript/Stage1ClearScene.cs
@@ -2,10 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class Stage1ClearScene : MonoBehaviour
 {
-    private float stayTime = 0f;
+    private float StayTime = 0f;
+
+    [SerializeField] private VideoPlayer ClearVideo;
+
+    [SerializeField] private RawImage RawImage;
+
+    void OnEnable()
+    {
+        ClearVideo.prepareCompleted += PrepareCompleted;
+    }
+
+    void OnDisable()
+    {
+        ClearVideo.prepareCompleted -= PrepareCompleted;
+    }
+
+    // 2. ロードを待つ
+    private void PrepareCompleted(VideoPlayer vp)
+    {
+        if (vp != ClearVideo) { return; }
+        StartCoroutine(ShowMovie(vp));
+    }
+
+    private IEnumerator ShowMovie(VideoPlayer vp)
+    {
+        // 1. 動画再生前にRawImageを非表示にする
+        RawImage.enabled = false;
+
+        // 2. 動画再生
+        vp.Play();
+
+        // 3. 動画の再生後1秒待機
+        yield return new WaitForSeconds(1f);
+
+        // 4. 動画のテクスチャを設定
+        RawImage.texture = vp.texture;
+
+        // 5. 動画の最初のフレームが進むまで待機
+        while (vp.frame < 1)
+        {
+            yield return null;
+        }
+
+        // 6. 動画の最初のフレームが準備できたらRawImageを有効にして表示
+        RawImage.enabled = true;
+    }
+
+
+    public void Play()
+    {
+        // 1. 動画プリロード
+        ClearVideo.Prepare();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +70,9 @@ public class Stage1ClearScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        stayTime += Time.deltaTime;
+        StayTime += Time.deltaTime;
 
-        if (stayTime > 8f)
+        if (StayTime > 8f)
         {
             SceneManager.LoadScene("GameScene");
         }
