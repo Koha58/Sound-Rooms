531fa2e5bf2c6fa770400f81d01d1ec22abb6879
STUDENT\2312004 <85koha90@gmail.com>
2025-01-10 16:44:07 +0900

    ClearScene冒頭の空白フレームを修正。

--- Assets/otherSceneScript/Stage1ClearScene.cs
+++ Assets/otherSceneScript/Stage1ClearScene.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,64 +9,79 @@ public class Stage1ClearScene : MonoBehaviour
     private float StayTime = 0f;
 
     [SerializeField] private VideoPlayer ClearVideo;
-
     [SerializeField] private RawImage RawImage;
+    [SerializeField] private RawImage FirstImage; // 0.5秒間表示する別の画像
 
     void OnEnable()
     {
+        // 動画準備完了時のイベントリスナー
         ClearVideo.prepareCompleted += PrepareCompleted;
+        // フレームが準備できた時に呼ばれるイベント
+        ClearVideo.frameReady += FrameReady;
     }
 
     void OnDisable()
     {
         ClearVideo.prepareCompleted -= PrepareCompleted;
+        ClearVideo.frameReady -= FrameReady;
     }
 
-    // 2. ロードを待つ
+    // 動画の準備が完了したときに呼ばれる
     private void PrepareCompleted(VideoPlayer vp)
     {
         if (vp != ClearVideo) { return; }
         StartCoroutine(ShowMovie(vp));
     }
 
+    // フレームが準備できた時に呼ばれる
+    private void FrameReady(VideoPlayer vp, long frameIndex)
+    {
+        // 最初のフレームが準備できたらRawImageを表示
+        if (frameIndex == 0)
+        {
+            RawImage.texture = vp.texture;
+            RawImage.enabled = true;
+        }
+    }
+
     private IEnumerator ShowMovie(VideoPlayer vp)
     {
+        // 最初の1秒間、FirstImage（別の画像）を表示
+        FirstImage.enabled = true;
+
+        // 0.5秒後にFirstImageを非表示にする
+        yield return new WaitForSeconds(0.5f);
         // 1. 動画再生前にRawImageを非表示にする
         RawImage.enabled = false;
 
-        // 2. 動画再生
+        // 動画の再生を開始
         vp.Play();
 
-        // 3. 動画の再生後1秒待機
-        yield return new WaitForSeconds(1f);
+        // 動画が最初に準備されるまで待機
+        yield return new WaitUntil(() => vp.isPrepared);
 
-        // 4. 動画のテクスチャを設定
-        RawImage.texture = vp.texture;
-
-        // 5. 動画の最初のフレームが進むまで待機
+        // 動画の最初のフレームが描画されるまで待機
         while (vp.frame < 1)
         {
             yield return null;
         }
 
-        // 6. 動画の最初のフレームが準備できたらRawImageを有効にして表示
+        // 動画の最初のフレームが準備できたらRawImageを有効にして表示
+        RawImage.texture = vp.texture;
         RawImage.enabled = true;
     }
 
-
     public void Play()
     {
-        // 1. 動画プリロード
+        // 動画プリロード
         ClearVideo.Prepare();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        // 初期化処理
     }
 
-    // Update is called once per frame
     void Update()
     {
         StayTime += Time.deltaTime;
@@ -76,6 +90,5 @@ public class Stage1ClearScene : MonoBehaviour
         {
             SceneManager.LoadScene("GameScene");
         }
-
     }
 }
--- Assets/otherSceneScript/TutorialClearScene.cs
+++ Assets/otherSceneScript/TutorialClearScene.cs
@@ -10,58 +10,77 @@ public class TutorialClearScene : MonoBehaviour
     private float StayTime = 0f;
 
     [SerializeField] private VideoPlayer ClearVideo;
-
     [SerializeField] private RawImage RawImage;
+    [SerializeField] private RawImage FirstImage; // 0.5秒間表示する別の画像
 
     void OnEnable()
     {
+        // 動画準備完了時のイベントリスナー
         ClearVideo.prepareCompleted += PrepareCompleted;
+        // フレームが準備できた時に呼ばれるイベント
+        ClearVideo.frameReady += FrameReady;
     }
 
     void OnDisable()
     {
         ClearVideo.prepareCompleted -= PrepareCompleted;
+        ClearVideo.frameReady -= FrameReady;
     }
 
-    // 2. ロードを待つ
+    // 動画の準備が完了したときに呼ばれる
     private void PrepareCompleted(VideoPlayer vp)
     {
         if (vp != ClearVideo) { return; }
         StartCoroutine(ShowMovie(vp));
     }
 
+    // フレームが準備できた時に呼ばれる
+    private void FrameReady(VideoPlayer vp, long frameIndex)
+    {
+        // 最初のフレームが準備できたらRawImageを表示
+        if (frameIndex == 0)
+        {
+            RawImage.texture = vp.texture;
+            RawImage.enabled = true;
+        }
+    }
+
     private IEnumerator ShowMovie(VideoPlayer vp)
     {
-        // 3. 動画再生
+        // 最初の1秒間、FirstImage（別の画像）を表示
+        FirstImage.enabled = true;
+
+        // 0.5秒後にFirstImageを非表示にする
+        yield return new WaitForSeconds(0.5f);
+        // 1. 動画再生前にRawImageを非表示にする
+        RawImage.enabled = false;
+
+        // 動画の再生を開始
         vp.Play();
 
-        // 4. フレームを待つ
+        // 動画が最初に準備されるまで待機
+        yield return new WaitUntil(() => vp.isPrepared);
+
+        // 動画の最初のフレームが描画されるまで待機
         while (vp.frame < 1)
         {
             yield return null;
         }
 
-        // 5. 画面に表示
+        // 動画の最初のフレームが準備できたらRawImageを有効にして表示
         RawImage.texture = vp.texture;
         RawImage.enabled = true;
     }
 
-    private void SetVideoUrl(string url)
-    {
-        ClearVideo.source = VideoSource.Url;
-        ClearVideo.url = url;
-    }
-
     public void Play()
     {
-        // 1. 動画プリロード
+        // 動画プリロード
         ClearVideo.Prepare();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        // 初期化処理
     }
 
     // Update is called once per frame
