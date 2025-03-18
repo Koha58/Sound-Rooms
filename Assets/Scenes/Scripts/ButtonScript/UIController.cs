d246ea2bbe710ab5b9c83ea7da3f2413999cf177
STUDENT\2312002 <tkondo591@gmail.com>
2025-03-18 15:00:07 +0900

    チュートリアルステージのUIをキーボード版は短くするように修正。

---
+++ Assets/Scenes/Scripts/ButtonScript/UIController.cs
@@ -0,0 +1,109 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+public class UIController : MonoBehaviour
+{
+    //歯車ボタン（設定画面に行くボタン）,メニュー画面,設定画面,操作説明画面,タイトルに戻る画面
+    [SerializeField] GameObject originSettingButton, menu, settingPanel1,settingPanel2,settingPanel3;
+
+    //ゲーム内設定を変更する画面へ遷移するボタン,操作説明画面へ遷移するボタン,タイトル画面へ遷移するボタン
+    [SerializeField] GameObject settingButton,qperationExplanationButton,backTitleButton;
+
+   //マイクスライダー,BGMスライダー,SEスライダー,マウススライダー
+   [SerializeField] Slider MicSlider, BGMSlider, SESlider, MouseSlider;
+
+    [SerializeField] AudioMixer audioMixer; //オーディオミキサー
+    [SerializeField] GameObject micObject;　//
+    public CinemachineFreeLook VCamera;     //
+
+    // パネル管理用リスト
+    private List<GameObject> panels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // パネルリストに追加
+        panels = new List<GameObject> {menu, settingPanel1, settingPanel2, settingPanel3 };
+
+        // 初期状態で全パネルを非表示
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void Menu(GameObject menuPanel)
+    {
+        menuPanel.SetActive(true);
+
+        Time.timeScale = 0;
+    }
+
+    public void CloseMenu(GameObject menuPanel)
+    {
+        menuPanel.SetActive(false);
+
+        // 初期状態で全パネルを非表示
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false);
+        }
+
+        Time.timeScale = 1;
+    }
+
+    public void SettingPanel()
+    {
+        settingPanel1.SetActive(true);
+        settingPanel2.SetActive(false);
+        settingPanel3.SetActive(false);
+    }
+    public void SettingPanel1()
+    {
+        settingPanel1.SetActive(false);
+        settingPanel2.SetActive(true);
+        settingPanel3.SetActive(false);
+    }
+    public void SettingPanel2()
+    {
+        settingPanel1.SetActive(false);
+        settingPanel2.SetActive(false);
+        settingPanel3.SetActive(true);
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void SetBGM(float volume2)
+    {
+        audioMixer.SetFloat("BGM", volume2);
+    }
+
+    public void SetSE(float volume3)
+    {
+        audioMixer.SetFloat("SE", volume3);
+    }
+
+    public void SetMic(float volume)
+    {
+        AudioSource Mic = micObject.GetComponent<AudioSource>();
+        Mic.volume = MicSlider.value;
+    }
+
+    public void SetMouse(float level1)
+    {
+        VCamera.m_YAxis.m_MaxSpeed = MouseSlider.value / 50;
+        VCamera.m_XAxis.m_MaxSpeed = MouseSlider.value * 50;
+    }
+}
